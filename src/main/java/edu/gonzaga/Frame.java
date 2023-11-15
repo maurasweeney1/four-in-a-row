8345a9628d805de7ec1b6c448cf8a71d30eae3ea
maurasweeney1 <123509194+maurasweeney1@users.noreply.github.com>
2023-11-15 12:22:16 -0800

    added basic frame class (#1)

---
+++ src/main/java/edu/gonzaga/Frame.java
@@ -0,0 +1,13 @@
+package edu.gonzaga;
+
+import java.awt.*;
+import javax.swing.*;
+
+class Frame extends JFrame {
+    private static final int DEFAULT_WIDTH = 300;
+    private static final int DEFAULT_HEIGHT = 200;
+
+    public Frame() {
+        setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+    }
+}
