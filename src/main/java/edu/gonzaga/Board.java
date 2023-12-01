47971c7b6992164c5847e5c8210829dd9f89d478
abbyhid00 <abbyhidalgo03@gmail.com>
2023-11-30 19:05:04 -0800

    fixed out of range errors that were occuring

--- src/main/java/edu/gonzaga/Board.java
+++ src/main/java/edu/gonzaga/Board.java
@@ -72,21 +72,26 @@ public class Board {
                 int placeOnBoard = board[i][j];
                 if (placeOnBoard == 0) {
                     continue;
-                } else if (j + 3 < width && placeOnBoard == board[i][j + 1] && placeOnBoard == board[i][j + 2]
-                        && placeOnBoard == board[i][j + 3]) { // looks right to see if there is four in a row
-                    return true;
-                } else if (i > 0 && placeOnBoard == board[i - 1][j] && placeOnBoard == board[i - 2][j]
-                        && placeOnBoard == board[i - 3][j]) { // looks up
-                    return true;
-                } else if (j + 3 < width && i > 0 && placeOnBoard == board[i - 1][j + 1]
-                        && placeOnBoard == board[i - 2][j + 2]
-                        && placeOnBoard == board[i - 3][j + 3]) { // looks up and to the right
-                    return true;
-                } else if (j - 3 >= 0 && i > 0 && placeOnBoard == board[i - 1][j - 1]
-                        && placeOnBoard == board[i - 2][j - 2]
-                        && placeOnBoard == board[i - 3][j - 3]) { // looks up and to the left
-
-                    return true;
+                } else if (j + 3 < width) { // looks right to see if there is four in a row
+                    if( placeOnBoard == board[i][j + 1] && placeOnBoard == board[i][j + 2]
+                            && placeOnBoard == board[i][j + 3]){
+                        return true;
+                    }
+                } else if (i > 0 ) { // looks up
+                    if(placeOnBoard == board[i - 1][j] && placeOnBoard == board[i - 2][j]
+                            && placeOnBoard == board[i - 3][j]){
+                        return true;
+                    }
+                } else if (j + 3 < width && i > 0) { // looks up and to the right
+                    if (placeOnBoard == board[i - 1][j + 1] && placeOnBoard == board[i - 2][j + 2]
+                            && placeOnBoard == board[i - 3][j + 3]){
+                        return true;
+                    }
+                } else if (j - 3 >= 0 && i > 0) { // looks up and to the left
+                    if(placeOnBoard == board[i - 1][j - 1] && placeOnBoard == board[i - 2][j - 2]
+                            && placeOnBoard == board[i - 3][j - 3]){
+                        return true;
+                    }
                 }
             }
         }
